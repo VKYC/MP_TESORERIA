825b416f0646508ffe568f315f7af9ca36fb8df1
Jhon Jairo Rojas Ortiz <jhon.rojas@nextpro.pe>
2023-11-07 23:23:59 +0000

    bank_ids

--- payroll_payment/wizards/payroll_payment_wizard.py
+++ payroll_payment/wizards/payroll_payment_wizard.py
@@ -11,7 +11,7 @@ class PayrollPaymentWizard(models.Model):
     def process_payroll(self):
         move_ids = self.env.context.get('active_ids', [])
         moves = self.env['account.move'].browse(move_ids)
-        moves_to_process = moves.filtered(lambda move: move.for_payroll and move.payment_state == 'not_paid' and move.partner_bank_id and not move.partner_id.blocked_for_payments and move.partner_id.is_payroll)
+        moves_to_process = moves.filtered(lambda move: move.for_payroll and move.payment_state == 'not_paid' and len(move.partner_id.bank_ids) > 0 and not move.partner_id.blocked_for_payments and move.partner_id.is_payroll)
         moves_to_process.write({
                 # 'for_payroll': True,
                 'payroll_payment_id': self.payroll_payment_id.id
